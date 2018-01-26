182b241bd95329cba926a4b4b8f4f38504ab1fd7
PratapRaj056 <!@8090437c@$>
2018-01-26 17:59:09 +0530

    Show hide added!

--- app/src/main/java/com/example/adt/demo/MainActivity.kt
+++ app/src/main/java/com/example/adt/demo/MainActivity.kt
@@ -7,27 +7,44 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var isBoolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val isBoolean = false
+        val list = ArrayList<Adapter.AttData>()
+        list.add(Adapter.AttData("VipLove", "90%"))
+        list.add(Adapter.AttData("VipLove2", "90%"))
+        list.add(Adapter.AttData("VipLove3", "90%"))
+        list.add(Adapter.AttData("VipLove4", "90%"))
+        list.add(Adapter.AttData("VipLove5", "90%"))
+        recycler_view.adapter = Adapter(list)
+
+        down.setOnClickListener{ onClickDown() }
+
+        showHideRecycler()
+
+    }
+
+    private fun onClickDown(){
+        if(recycler_view.visibility == View.VISIBLE){
+            isBoolean = false
+        }else if(recycler_view.visibility == View.GONE){
+            isBoolean = true
+        }
+
+        showHideRecycler()
+
+    }
+
+    private fun showHideRecycler(){
         if(isBoolean){
             recycler_view.visibility = View.VISIBLE
-
-            val list = ArrayList<Adapter.AttData>()
-
-            list.add(Adapter.AttData("VipLove", "90%"))
-            list.add(Adapter.AttData("VipLove2", "90%"))
-            list.add(Adapter.AttData("VipLove3", "90%"))
-            list.add(Adapter.AttData("VipLove4", "90%"))
-            list.add(Adapter.AttData("VipLove5", "90%"))
-
             recycler_view.setHasFixedSize(true)
-            recycler_view.adapter = Adapter(list)
+
         }else{
             recycler_view.visibility = View.GONE
         }
-
     }
 }
